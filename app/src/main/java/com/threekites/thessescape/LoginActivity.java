682f44a01d3b86ecea4a7efe385eb9dabd041c6b
Mats <mats@softweb.gr>
2019-12-27 20:24:38 +0200

    Sign In Button. Put logic

--- app/src/main/java/com/threekites/thessescape/LoginActivity.java
+++ app/src/main/java/com/threekites/thessescape/LoginActivity.java
@@ -3,12 +3,39 @@ package com.threekites.thessescape;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText emailEditText, passwordEditText;
+    private String username = "test@test.gr", password = "1234";
+    private Button signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
+        initViews();
+        setUpListeners();
     }
+
+    private void initViews() {
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        signInButton = findViewById(R.id.signInButton);
+    }
+
+    private void setUpListeners() {
+        signInButton.setOnClickListener(v -> {
+            if (emailEditText.getText().toString().equals(username) && passwordEditText.getText().toString().equals(password)) {
+                Toast.makeText(LoginActivity.this, "Login Successfully!", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(LoginActivity.this, "Login Error! WRONG CREDENTIALS", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
